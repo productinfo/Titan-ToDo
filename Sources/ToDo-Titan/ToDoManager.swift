9776f97227c91a05271d3ca878f7ced0d736be06
Ryan Collins <ryan@ryanmcollins.com>
2017-07-17 17:10:02 -0400

    Updated ToDoManager with new No-Parameter Query Type

--- Sources/ToDo-Titan/ToDoManager.swift
+++ Sources/ToDo-Titan/ToDoManager.swift
@@ -28,7 +28,7 @@ struct ToDoManager {
         var result = true
         
         do  {
-            try PSQL().query("DELETE FROM items", params: [])
+            try PSQL().query("DELETE FROM items")
         } catch {
             result = false
         }
@@ -36,6 +36,43 @@ struct ToDoManager {
         return result
     }
     
-    
+    func read() -> [String: Any]? {
+        
+        var returnValue = [String: Any]()
+        var items = [[String: Any]]()
+        
+        do {
+            try PSQL().query("SELECT * FROM ITEMS") {
+                result in
+                
+                for row in result.rows() {
+                    
+                    dump(row)
+                    
+                    //Items MUST have an ID AND a title
+                    if let id = row["id"] as? Int, let title = row["title"] as? String {
+                        
+                        var item = [String: Any]()
+                        
+                        item["id"] = id
+                        item["title"] = title
+                    
+                        items.append(item)
+                    }
+                }
+            }
+        } catch {
+            return nil
+        }
+        
+        if !items.isEmpty {
+            returnValue["items"] = items
+        }
+        
+        dump(returnValue)
+        
+        return returnValue
+        
+    }
     
 }
