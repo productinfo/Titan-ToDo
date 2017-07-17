afc63b04150bac88ce883f0f687a6dd84d5a1ce4
Ryan Collins <ryan@ryanmcollins.com>
2017-07-17 16:15:11 -0400

    Added CORS Support

--- Package.swift
+++ Package.swift
@@ -6,6 +6,8 @@ let package = Package(
     name: "ToDo-Titan",
     dependencies: [
         .Package(url: "https://github.com/bermudadigitalstudio/Titan.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/bermudadigitalstudio/TitanKituraAdapter.git", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/bermudadigitalstudio/TitanKituraAdapter.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/bermudadigitalstudio/Rope.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/bermudadigitalstudio/TitanCORS.git", majorVersion: 0),
     ]
 )
