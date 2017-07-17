77f445b3ad9a7ce5bb45144e21e171d35c7cc0c4
Ryan Collins <ryan@ryanmcollins.com>
2017-07-17 16:17:05 -0400

    Added a Database Manager
    Added a database management struct to contain the functions needed to modify the database, started with add & delete.

---
+++ Sources/ToDo-Titan/ToDoManager.swift
@@ -0,0 +1,39 @@
+//
+//  ToDo.swift
+//  ToDo-Titan
+//
+//  Created by Ryan Collins on 7/17/17.
+//
+//
+
+import Rope
+
+struct ToDoManager {
+    
+    func add(json: String) -> Bool {
+        
+        var result = true
+        
+        do {
+            try PSQL().query("INSERT INTO items (data) VALUES ($1)", params: [json])
+        } catch {
+            result = false
+        }
+        
+        return result
+    }
+    
+    func delete() -> Bool {
+        
+        var result = true
+        
+        do  {
+            try PSQL().query("DELETE FROM items", params: nil)
+        } catch {
+            result = false
+        }
+        
+        return result
+    }
+    
+}
