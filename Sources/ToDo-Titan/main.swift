d5deaf8c8bf85fee9c066ce4dbd7dcf17fd54229
Ryan Collins <ryan@ryanmcollins.com>
2017-07-17 18:45:35 -0400

    Routing Cleanup
    Moved API Routes to its Own File to Keep Code Clean

--- Sources/ToDo-Titan/main.swift
+++ Sources/ToDo-Titan/main.swift
@@ -1,75 +1,11 @@
 import Titan
 import TitanKituraAdapter
 import TitanCORS
-import Foundation
 
 let app = Titan()
 
-/// Default Route
-app.get("/") { req, _ in
-    let jsonRepresentation = ToDoManager().getAll()
-    
-    var response = "{ }"
-    
-    if let dict = jsonRepresentation {
-        //Our query ran successfully, we should ahve at least an empty array to work with
-        
-        do {
-            let json = try JSONSerialization.data(withJSONObject: dict, options: .prettyPrinted)
-            if let jsonString = String(data: json, encoding: .utf8) {
-                response = jsonString
-            }
-            
-        } catch {
-            return (req, Response(500))
-        }
-    }
-    
-    //Something Failed On Our End, Send Back an Error
-    return (req, Response(200, response, [Header(name: "Content-Type", value: "application/json")]))
-}
-
-/// Accept New ToDo Items
-app.post("/") { req, _ in
-    
-    //Validate JSON
-    guard var dict = req.json as? [String: Any] else {
-        return (req, Response(400))
-    }
-    
-    //Get String Representations to Store in Postgres
-    let json = req.body
-    
-    var success = true
-    
-    //Add to Database, Check for Success
-    if let id = ToDoManager().add(json: json) {
-        dict["url"] = "/item/\(id)/"
-    }
-    
-    dict["success"] = true
-    dict["completed"] = false
-
-    if let data = try? JSONSerialization.data(withJSONObject: dict, options: .prettyPrinted) {
-        if let response = String(data: data, encoding: .utf8) {
-            return(req, Response(200, response, [Header(name: "Content-Type", value: "application/json")]))
-        }
-    }
-    
-    //Bad json was sent to us, tell them it's a bad request
-    return (req, Response(400))
-}
-
-/// Delete the ToDos
-app.delete("/") { req, _ in
-    
-    if ToDoManager().delete() {
-        return(req, Response(200))
-    }
-    
-    //If delete fails, return a server error
-    return(req, Response(500))
-}
+/// Add Our API Routes
+Router().makeRoutes(app)
 
 /// Handle 404 Errors
 func send404IfNoMatch(req: RequestType, res: ResponseType) -> (RequestType, ResponseType) {
